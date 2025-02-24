ff9e62647655bb66c09ea825c11117be8dc5140c
tskoyo <jovansr@protonmail.com>
2025-02-24 21:17:13 +0100

    inital commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	client "github.com/celestiaorg/celestia-openrpc"
+)
+
+func main() {
+	rootCtx := context.Background()
+	addr := "http://192.168.0.56:26658"
+	token := ''
+
+	client, err := client.NewClient(rootCtx, addr, token)
+	if err != nil {
+		panic(err)
+	}
+
+	defer client.Close()
+
+	peerAddr, err := client.P2P.Info(rootCtx)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Peer addr: ", peerAddr.ID.String())
+}
